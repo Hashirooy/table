8f4e8a277b86a3606958ed0a94fa637676a3e94f
Hashirooy <battlenetruss@gmail.com>
2026-04-18 12:44:19 +0400

    last

--- src/shared/helper/sort.ts
+++ src/shared/helper/sort.ts
@@ -1,9 +1,8 @@
 import type { Order, SortColumn } from "../ui/Table/type";
 
 export const sortFunc = (arr: {}[], column: string, sortColumn: SortColumn) => {
-  console.log(arr);
   const q = column.toString().toLowerCase();
-  console.log(q);
+
   if (sortColumn === "asc") {
     return [...arr].sort((a: any, b: any) => {
       if (a.properties[q] === undefined || b.properties[q] === undefined) {
